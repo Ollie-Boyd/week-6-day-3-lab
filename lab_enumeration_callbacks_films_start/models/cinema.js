eb04341c4dc9d537f421d33b47ec5e54f6b21ed1
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-06-17 14:34:37 +0100

    passed first 4 tests

--- lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -2,4 +2,35 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmTitles = function () {
+  const titles = [];
+  this.films.forEach((film) => {
+    titles.push(film.title);
+  })
+  return titles;
+  
+}
+
+Cinema.prototype.filmsFound = function (title) {
+  const found = this.films.find(film => film.title === title);
+  return found;
+}
+
+Cinema.prototype.filterByGenre = function(genre) {
+  const filmsMatchingGenre = this.films.filter(film => film.genre === genre)
+  return filmsMatchingGenre
+}
+
+
+
+
+
+
+  // const titles = this.films.map((film)=>{
+  //   return film.title
+  // })
+
+  // return titles
+
+
 module.exports = Cinema;
--- lab_enumeration_callbacks_films_start/specs/cinema_spec.js
+++ lab_enumeration_callbacks_films_start/specs/cinema_spec.js
@@ -28,10 +28,26 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, films);
   });
 
-  it('should be able to get a list of film titles');
-  it('should be able to find a film by title');
-  it('should be able to filter films by genre');
-  it('should be able to check whether there are some films from a particular year');
+  it('should be able to get a list of film titles', function () {
+    const actual = cinema.filmTitles();
+    assert.deepStrictEqual(actual, ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting']);
+  });
+
+  it('should be able to find a film by title', function () {
+    const actual = cinema.filmsFound('Moonlight')
+    assert.deepStrictEqual(actual, moonlight)
+  });
+
+
+  it('should be able to filter films by genre', function(){
+    const actual = cinema.filterByGenre('drama')
+    assert.deepStrictEqual(actual, [moonlight, trainspotting])
+  });
+
+
+  it('should be able to check whether there are some films from a particular year', function () {
+    const actual = 
+  });
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
